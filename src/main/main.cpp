b900d0ef12467077dd470627b83ba6a684ae3ad6
deltathecreatorr <julio_anandaraaj@outlook.com>
2026-03-07 21:04:42 +0000

    Update CI/CD pipeline test message for clarity

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,7 +2,7 @@
 #include <slam/slam.h>
 
 int main() {
-    fmt::print("Is this test for the CI/CD pipeline working?\n");
+    fmt::print("Is this test for the CI/CD pipeline working rn?\n");
     slam();
     return 0;
 }
