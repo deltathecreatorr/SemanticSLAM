7e99c9c8ac8114eef6f871f276bf77f1f277e0b8
deltathecreatorr <julio_anandaraaj@outlook.com>
2026-04-04 14:03:17 +0100

    Add lidar module: create lidar.cpp and lidar.h files; update .gitignore to include build and robot_env directories

---
+++ src/lidar/lidar.cpp
@@ -0,0 +1,7 @@
+#include "lidar.h"
+#include <iostream>
+#include <fmt/core.h>
+#include <thread>
+#include <chrono>
+#include <lgpio.h>
+
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#
 
 #include "movementScripts/movementScripts.h"
 
